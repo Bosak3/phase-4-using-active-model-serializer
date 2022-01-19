3c6b94b861c164d1cf98638b4ba7d207e89aa1a9
Adam <bosak3@gmail.com>
2022-01-18 19:07:01 -0600

    commit tree 5b2c9c83feda8075de3225daca1d506587828a75

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,11 +6,16 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
-  def show
-    movie = Movie.find(params[:id])
-    render json: movie
-  end
+ # app/controllers/movies_controller.rb
+def show
+  movie = Movie.find(params[:id])
+  render json: movie
+end
 
+def summary
+  movie = Movie.find(params[:id])
+  render json: movie, serializer: MovieSummarySerializer
+end
   private
 
   def render_not_found_response
